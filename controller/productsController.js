bd3566f7519f4326ff7e0d0e55b0b0cebd7ce987
uprajnambhat <uprajnambhat96@gmail.com>
2024-06-18 14:42:10 +0530

    updated

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,25 +1,5 @@
-const { getDetails } = require("../getDetails");
-const connection = require("../db");
-
 const getProdDetails = (req, res) => {
   console.log("reached here");
-  const query = "SELECT * FROM prodDetails";
-
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error("Error executing query:", error.stack);
-      res.status(500).send("Server Error");
-      return;
-    }
-
-    // Log the raw data packet for debugging
-    console.log("Raw data packet:", results);
-
-    // Process the results as needed
-    res.json(results);
-  });
-
-  console.log("rcb", getDetails());
   res.send({
     products: {
       data: {
